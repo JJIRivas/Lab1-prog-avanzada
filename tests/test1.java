71d924a47773eb7def2337829f70228139ff5049
Juan <j.inzunza05@ufromail.cl>
2026-03-25 11:18:35 -0300

    v

--- tests/test1.java
+++ tests/test1.java
@@ -2,13 +2,18 @@ import org.junit.jupiter.api.Test;
 
 public class prueba {
 
+    @Test
     void inptVacio() {}
 
+    @Test
     void elemRepet() {}
 
+    @Test
     void inptNoInt() {}
 
+    @Test
     void inptMasDeUno() {}
 
+    @Test
     void elemYaOrdenados() {}
 }
