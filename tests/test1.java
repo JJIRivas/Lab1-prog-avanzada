4f8a72a202164520ffdaac028c3159113f64df45
Juan <j.inzunza05@ufromail.cl>
2026-03-25 11:12:47 -0300

    Archivo prueba basicas

---
+++ tests/test1.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+public class prueba {
+
+    void inptVacio() {}
+
+    void elemRepet() {}
+
+    void inptNoInt() {}
+
+    void inptMasDeUno() {}
+
+    void elemYaOrdenados() {}
+}
