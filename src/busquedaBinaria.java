91de37abd37a3ead29273ba8b8df0d0127ab3700
Juan <j.inzunza05@ufromail.cl>
2026-03-25 10:06:53 -0300

    Se implemento busqueda binaria basica.

--- src/busquedaBinaria.java
+++ src/busquedaBinaria.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -6,7 +7,6 @@ class Binario {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Random numGen = new Random();
 
         System.out.print("Introduce el numero de filas (n): ");
 
@@ -16,8 +16,71 @@ class Binario {
 
         int col = sc.nextInt();
 
-        int[][] matriz = new int[fil][col];
+        System.out.println("\n" + "Matriz creada: " + "\n");
+
+        int[][] matriz = arrSetup(fil, col);
+
+        System.out.print("\n" + "Numero a buscar: ");
+        System.out.println(
+            "Numero encontrado en la posicion: " +
+                busquedaBin(sc.nextInt(), matriz)
+        );
 
         sc.close();
+    }
+
+    public static int busquedaBin(int n, int[][] matFinal) {
+        int filas = matFinal.length;
+        int colum = matFinal[0].length;
+        int min = 0;
+        int max = filas * colum - 1;
+
+        while (min <= max) {
+            int med = min + (max - min) / 2;
+            /*System.out.println(
+                "min: " + min + "\n med: " + med + "\n max: " + max
+                );*/
+            int medValue = matFinal[med / colum][med % colum];
+            // System.out.println("x: " + (med / colum) + " y: " + (med % colum));
+
+            if (medValue == n) return med + 1;
+            if (medValue < n) min = med + 1;
+            else max = med - 1;
+        }
+
+        return -1;
+    }
+
+    public static int[][] arrSetup(int fil, int col) {
+        Random numGen = new Random();
+        int[][] mat = new int[fil][col];
+
+        for (int i = 0; i < fil; i++) {
+            for (int j = 0; j < col; j++) {
+                mat[i][j] = numGen.nextInt(1001);
+            }
+        }
+
+        int[] arr = new int[fil * col];
+
+        int index = 0;
+        for (int i = 0; i < fil; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[index++] = mat[i][j];
+            }
+        }
+
+        index = 0;
+        Arrays.sort(arr);
+
+        for (int i = 0; i < fil; i++) {
+            for (int j = 0; j < col; j++) {
+                mat[i][j] = arr[index++];
+                System.out.print("\t" + mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        return mat;
     }
 }
